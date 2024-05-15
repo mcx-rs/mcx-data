b244255695a16ff364a17a90abe41a7779ab5fa5
Logiase Song <logiase.song@gmail.com>
2024-05-15 14:05:23 +0800

    Update scripts

--- scripts/peripherals.py
+++ scripts/peripherals.py
@@ -8,11 +8,58 @@ from typing import Dict, List
 
 import yaml
 
-with open("peripherals.yaml", "r") as f:
-    _peripherals: Dict[str, Dict[str, List[str]]] = yaml.safe_load(f)
+
+class PeripheralMapping(object):
+    def __init__(self, f: str, mapping: Dict[str, List[str]]) -> None:
+        self.f = f
+        self.mapping = mapping
+
+    def contain_device(self, device: str) -> List[str] | None:
+        for r, opnames in self.mapping.items():
+            if len(re.findall(r, device)) != 0:
+                return opnames
+        return None
+
+    @classmethod
+    def from_dict(cls, dict) -> "PeripheralMapping":
+        return cls(dict["from"], dict["mapping"])
+
+    def __str__(self) -> str:
+        return f"from: {self.f}, mapping: {self.mapping}"
+
+
+class PeripheralMappings(object):
+    def __init__(self, mappings: Dict[str, PeripheralMapping]) -> None:
+        self.mappings = mappings
+
+    def get_mapped_peripheral_name(self, device: str, block: str) -> str | None:
+        for pname, mapping in self.mappings.items():
+            opnames = mapping.contain_device(device)
+            if opnames == None:
+                continue
+            for opname in opnames:
+                if opname == block:
+                    return pname
+        return None
+
+    @classmethod
+    def parse(cls, path="./peripherals.yaml") -> "PeripheralMappings":
+        with open(path, "r") as f:
+            d = yaml.safe_load(f)
+        for k, v in d.items():
+            d[k] = PeripheralMapping.from_dict(v)
+        return cls(d)
+
+    def __str__(self) -> str:
+        s = ""
+        for k, m in self.mappings.items():
+            s += f"{k}: {m}\n"
+        s.removesuffix("\n")
+        return s
 
 
 def get_device_mapped_peripherals(device: str) -> List:
+    _peripherals = PeripheralMappings.parse()
     mapped_peripherals = []
     for pname, mapping in _peripherals.items():
         for r, opnames in mapping.items():
@@ -28,16 +75,14 @@ def get_device_mapped_peripherals(device: str) -> List:
 
 
 def copy_and_rename_peripherals():
+    _peripherals = PeripheralMappings.parse()
     os.makedirs("temp/peripherals", exist_ok=True)
-    for pname, mapping in _peripherals.items():
-        device = mapping["from"]
-        for r, opnames in mapping.items():
-            if r == "from":
-                continue
-            if len(re.findall(f"^{r}$", device)) != 1:
-                continue
-            op = opnames[0]
-            break
+    for pname, mapping in _peripherals.mappings.items():
+        device = mapping.f
+        op = mapping.contain_device(device)
+        if op is None:
+            continue
+        op = op[0]
         shutil.copy(f"temp/{device}/{op}.yaml", f"temp/peripherals/{pname}.yaml")
 
 
